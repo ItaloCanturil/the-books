e3d82af3a37583093751eeb2d3d73ad0f5f0673d
Italo Canturil <italo.cantur@gmail.com>
2025-01-10 15:26:19 -0300

    feat: add styles to responsive

--- src/components/ui/wrapper.tsx
+++ src/components/ui/wrapper.tsx
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 
-const Wrapper = styled.div<{ maxWidth?: string }>`
+const Wrapper = styled.div<{ maxWidth?: string, width?: string, height?: string, maxHeight?: string }>`
+  background-color: #edf2f4;
   padding: 1em;
   border-radius: 10px;
-  border: 1px solid #ccc;
-  max-width: ${props => props.maxWidth}
+  border: 1px solid #D9D9D9;
+  max-width: ${props => props.maxWidth};
+  width: ${props => props.width};
+  max-height: ${props => props.maxHeight};
+  height: ${props => props.height};
+  overflow: scroll;
 `;
 
 
